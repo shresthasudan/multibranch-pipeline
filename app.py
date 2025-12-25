1e35b32ac4018b708c2dcfa7a0b6edec512b7350
Sudan <shresthasudan18@gmail.com>
2025-12-25 09:11:13 +0545

    port changes

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello, from the Isolated Venv App!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000) # change the port here
+    app.run(host='0.0.0.0', port=4949) # change the port here
